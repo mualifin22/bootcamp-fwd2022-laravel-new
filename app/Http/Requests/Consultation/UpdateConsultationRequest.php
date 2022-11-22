ad45c2c553a2b555b4591839256cf60e59c913ff
mualifin22 <mimualifin22.com>
2022-11-22 09:46:47 +0700

    backup

---
+++ app/Http/Requests/Consultation/UpdateConsultationRequest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Requests\Consultation;
+
+use App\Models\MasterData\Consultation;
+// use Gate;
+use Illuminate\Foundation\Http\FormRequest;
+use Symfony\Component\HttpFoundation\Response;
+
+// this rule is only at update request 
+use Illuminate\Validation\Rule;
+
+class UpdateConsultationRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'name' => [
+                'required', 'string', 'max:255',
+            ]
+        ];
+    }
+}
---
+++ app/Http/Requests/User/UpdateUserRequest.php
@@ -0,0 +1,48 @@
+<?php
+
+use App\Models\User;
+// use Gate;
+use Illuminate\Foundation\Http\FormRequest;
+use Symfony\Component\HttpFoundation\Response;
+
+// this rule is only at update request 
+use Illuminate\Validation\Rule;
+
+class UpdateUserRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        // create middleware from kernel at here
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'name' => [
+                'required', 'string', 'max:255',
+            ],
+
+
+            'email' => [
+                'required', 'email', 'max:255', Rule::unique('users')->ignore($this->user),
+                // Rule unique only works for other record id
+            ],
+
+            'password' => [
+                'min:8', 'string', 'max:255', 'mixedcase', 
+            ],
+            // add validation for role this here    
+        ];
+    }
+}
