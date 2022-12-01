ab159ebc07a6174d717209f83bba1a931581dc7b
mualifin22 <mimualifin22@gmail.com>
2022-12-01 22:58:49 +0700

    update all for slicing

--- resources/views/includes/frontsite/style.blade.php
+++ resources/views/includes/frontsite/style.blade.php
@@ -2,4 +2,4 @@
 <link rel="preconnect" href="{{ url('https://fonts.gstatic.com') }}" crossorigin />
 <link href="{{ url('https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap" rel="stylesheet') }}" />
 
-<link rel="stylesheet" href="{{ asset('/assets/frontsite/styl.css') }}" />
+<link rel="stylesheet" href="{{ asset('/assets/frontsite/style.css') }}" />
