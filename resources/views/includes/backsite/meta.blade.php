9518470a61ee237448fa2f7edda8183e2655037d
mualifin22 <mimualifin22@gmail.com>
2022-12-27 11:40:08 +0700

    update includes

---
+++ resources/views/includes/backsite/meta.blade.php
@@ -0,0 +1,11 @@
+<meta http-equiv="content-type" content="text/html; charset=UTF-8">
+<meta http-equiv="x-ua-compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0, minimal=ui">
+
+{{-- seo setting base --}}
+<meta name="description" content="{{ 'Meet Your Doctor. Trusted & Professional.' }}">
+<meta key="keywords" content="{{ 'doctor, meetdoctor, patien, appointment, trusted & professional' }}">
+<meta name="author" content="{{ 'Meet Doctor' }}">
+
+<!-- CSFR Token -->
+<meta name="csrf-token" content="{{ csrf_token() }}">
